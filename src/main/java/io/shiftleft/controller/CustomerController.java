8f2a7b6abd305aa69962d05ee1af66c000d88450
sanket-qwiet <sanket.infracloud@qwiet.ai>
2024-12-30 18:48:44 +0530

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 24

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -161,14 +161,17 @@ public class CustomerController {
        * @param request
        * @return
        */
-      private boolean checkCookie(WebRequest request) throws Exception {
+private boolean checkCookie(WebRequest request) throws Exception {
       	try {
-			return request.getHeader("Cookie").startsWith("settings=");
+			return StringUtils.startsWith(request.getHeader("Cookie"), "settings=");
 		}
 		catch (Exception ex)
 		{
 			System.out.println(ex.getMessage());
 		}
+		return false;
+      }
+
 		return false;
       }
 
@@ -216,7 +219,7 @@ public class CustomerController {
    * @param request
    * @throws Exception
    */
-  @RequestMapping(value = "/saveSettings", method = RequestMethod.GET)
+@RequestMapping(value = "/saveSettings", method = RequestMethod.GET)
   public void saveSettings(HttpServletResponse httpResponse, WebRequest request) throws Exception {
     // "Settings" will be stored in a cookie
     // schema: base64(filename,value1,value2...), md5sum(base64(filename,value1,value2...))
@@ -227,25 +230,25 @@ public class CustomerController {
     }
 
     String settingsCookie = request.getHeader("Cookie");
-    String[] cookie = settingsCookie.split(",");
+    String[] cookie = StringUtils.split(settingsCookie, ",");
 	if(cookie.length<2) {
 	  httpResponse.getOutputStream().println("Malformed cookie");
       throw new Exception("cookie is incorrect");
     }
 
-    String base64txt = cookie[0].replace("settings=","");
+    String base64txt = StringUtils.replace(cookie[0], "settings=","");
 
     // Check md5sum
     String cookieMD5sum = cookie[1];
     String calcMD5Sum = DigestUtils.md5Hex(base64txt);
-	if(!cookieMD5sum.equals(calcMD5Sum))
+	if(!StringUtils.equals(cookieMD5sum, calcMD5Sum))
     {
       httpResponse.getOutputStream().println("Wrong md5");
       throw new Exception("Invalid MD5");
     }
 
     // Now we can store on filesystem
-    String[] settings = new String(Base64.getDecoder().decode(base64txt)).split(",");
+    String[] settings = StringUtils.split(new String(Base64.getDecoder().decode(base64txt)), ",");
 	// storage will have ClassPathResource as basepath
     ClassPathResource cpr = new ClassPathResource("./static/");
 	  File file = new File(cpr.getPath()+settings[0]);
@@ -257,11 +260,12 @@ public class CustomerController {
     // First entry is the filename -> remove it
     String[] settingsArr = Arrays.copyOfRange(settings, 1, settings.length);
     // on setting at a linez
-    fos.write(String.join("\n",settingsArr).getBytes());
-    fos.write(("\n"+cookie[cookie.length-1]).getBytes());
+    fos.write(StringUtils.join(settingsArr, "\n").getBytes());
+    fos.write((("\n"+cookie[cookie.length-1]).getBytes()));
     fos.close();
     httpResponse.getOutputStream().println("Settings Saved");
   }
+
 
   /**
    * Debug test for saving and reading a customer
